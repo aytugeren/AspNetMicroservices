fc36099e9b008f9f3b360a1292484a0d02857c71
Aytuğ Eren <aytgeren@gmail.com>
2022-05-09 23:30:50 +0300

    EventBus - RabbitMq implemented to the Basket.API

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,9 +1,12 @@
 ﻿namespace Basket.API.Controllers
 {
+    using AutoMapper;
     #region
     using Basket.API.Entities;
     using Basket.API.GrpcServices;
     using Basket.API.Repositories;
+    using EventBus.Messages.Events;
+    using MassTransit;
     using Microsoft.AspNetCore.Mvc;
     using System.Net;
     #endregion
@@ -17,17 +20,25 @@
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
         /// <summary>
         /// Ctor
         /// </summary>
-        /// <param name="repository">The <see cref="IBasketRepository"/></param>
-        /// <param name="discountGrpcService">The <see cref="DiscountGrpcService"/></param>
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        /// <param name="repository"></param>
+        /// <param name="discountGrpcService"></param>
+        /// <param name="mapper"></param>
+        /// <param name="publishEndpoint"></param>
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _repository = repository;
             _discountGrpcService = discountGrpcService;
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
+
+
 
         /// <summary>
         /// The Get Basket
@@ -75,6 +86,35 @@
         {
             await _repository.DeleteBasket(userName);
             return Ok();
+        }
+
+        /// <summary>
+        /// The Checkout
+        /// </summary>
+        /// <param name="basketCheckout">The <see cref="BasketCheckout"/></param>
+        /// <returns></returns>
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // get existing basket with total price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            // send checkout event to rabbitmq
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
+
+           //Remove the basket
+           await _repository.DeleteBasket(basket.UserName);
+
+            return Accepted();
         }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
@@ -10,15 +11,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Redis Connection
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+// Grpc Configuration
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     builder => builder.Address = new Uri(configuration.GetValue<string>("GrpcSettings:DiscountUrl")));
 
+
+/// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        //amqp protocol
+        //username:password@address
+        cfg.Host(configuration.GetValue<string>("EventBusSettings:HostAddress"));
+    });
+});
+
+// General Configuration
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<DiscountGrpcService>();
 
 var app = builder.Build();
