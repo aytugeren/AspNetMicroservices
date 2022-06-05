d524aa579c8c1398fb68f22a8779b949d4b73e2e
Aytuğ Eren <aytgeren@gmail.com>
2022-06-05 21:00:01 +0300

    ApiGatewayOcelot Aggregator is added and containirized.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,15 +1,18 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Logging.AddJsonConsole();
 builder.Logging.AddDebug();
 builder.Logging.AddConsole();
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
+builder.Configuration.AddJsonFile($"ocelot.{app.Environment.EnvironmentName}.json", true, true);
+
 app.MapGet("/", () => "Hello World!");
-
 await app.UseOcelot();
-
 app.Run();
